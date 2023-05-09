45dd2caa369661ac0996ded8cf2c2e0a56e8762a
Saranja Navaneethakumar <saranja2209@gmail.com>
2023-05-09 14:20:57 +0530

    100276: AddForgetPassword/ResetPassword Features-4

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -4,6 +4,7 @@ import { Router } from '@angular/router';
 import { SharedService } from '../shared.service';
 import data from './../../assets/data.json';
 import { HandledataService } from '../handledata.service';
+import { CustomvalidationService } from '../customvalidation.service';
 
 @Component({
   selector: 'app-login',
@@ -15,13 +16,38 @@ export class LoginComponent implements OnInit {
 
   loginform : FormGroup = new FormGroup ({});
   forgetpwform : FormGroup = new FormGroup ({});
+  resetpwform : FormGroup = new FormGroup({});
 
-  constructor(public fb:FormBuilder, private router: Router,private sharedService: SharedService,
-    private dataService:HandledataService)
-  {}
+  constructor(
+    public fb:FormBuilder, 
+    private router: Router,
+    private sharedService: SharedService,
+    private dataService:HandledataService,
+    private customvalidator:CustomvalidationService)
+  {
+    this.forgetpwform = this.fb.group(
+      {
+        email : new FormControl('', [Validators.required, Validators.email]),
+        passwordf : new FormControl('', Validators.compose([Validators.required, this.customvalidator.patternValidator()])),
+        confirmpasswordf : new FormControl('', Validators.required),
+      },
+      {validator: this.customvalidator.MatchPassword('passwordf', 'confirmpasswordf')}
+    )
 
+    this.resetpwform = this.fb.group({
+      email : new FormControl('', [Validators.required, Validators.email]),
+      oldpassword : new FormControl('', Validators.compose([Validators.required, this.customvalidator.patternValidator()])),
+      newpassword : new FormControl('', Validators.compose([Validators.required, this.customvalidator.patternValidator()])),
+      confirmpw :  new FormControl('', Validators.required),
+    },
+    {validator: this.customvalidator.MatchPassword('newpassword', 'confirmpw')})
+  }
+
+  
   ngOnInit(): void {
-   
+    this.dataService.getDataFromJsonFile().subscribe(data => {
+      localStorage.setItem('myData', JSON.stringify(data));
+    });
     this.loginform = this.fb.group(
       {
         username : new FormControl('', Validators.required),
@@ -29,55 +55,112 @@ export class LoginComponent implements OnInit {
       }
     )
   }
-
-  alertMessage!: string;
-  
+  failalert!: string;
+  successalert:string='';
 
   onSubmit(){
     const username = this.loginform.value.username;
     const password = this.loginform.value.password;
 
     const currentItems = this.dataService.getItem('items') || [];
-    
+    //try myData localstorage   
     var filterdata = currentItems.filter((item: { username: any; }) => item.username == username)
-
     if(username == data.username)
     {
-      if(username == data.username && password == data.password)
-      {
+      if(username == data.username && password == data.password){
         this.sharedService.changeMessage('You have successfully logged in!');
         this.sharedService.printuser(username);
         this.router.navigate(['/student']);
       }
-    
       else{
-        this.alertMessage = 'Invalid Login!';
+        this.successalert = 'Invalid Login!';
         this.loginform.reset();
       }
     }
     else
-    {
-
-      for (var i = 0; i < filterdata.length; i++) {
-        if(username == filterdata[i].username && password == filterdata[i].password)
-        {
+    { for (var i = 0; i < filterdata.length; i++) {
+        if(username == filterdata[i].username && password == filterdata[i].password){
           this.sharedService.changeMessage('You have successfully logged in!');
           this.sharedService.printuser(username);
           this.router.navigate(['/student']);
         }
-      
         else{
-          this.alertMessage = 'Invalid Login!';
+          this.failalert = 'Invalid Login!';
           this.loginform.reset();
         }
       }
     }
-    
   }
 
-  get loginForm()
+  onForget()
   {
+    const email = this.forgetpwform.value.email;
+    const passwordf = this.forgetpwform.value.passwordf;
+    const confirmpasswordf = this.forgetpwform.value.confirmpasswordf;
+
+    const currentItems = this.dataService.getItem('items') || [];
+
+    const index = currentItems.findIndex((item: any) => item.email == email);
+    
+    var filterdata = currentItems.filter((item: { email: any; }) => item.email == email)
+
+    if(email == data.email)
+    {
+        data.password=passwordf;
+        this.successalert = 'You have successfully updated password! '+data.username;
+    }
+    else if(index !== -1)
+    { 
+      currentItems[index].password=passwordf;
+      console.log(currentItems[index].password)
+      localStorage.setItem('items', JSON.stringify(currentItems));
+          this.successalert = 'You have successfully updated password! '+currentItems[index].username;
+    }
+    else{
+          this.failalert = 'Invalid User!';
+          this.forgetpwform.reset();
+    }
+  }
+    
+  onresetpw()
+  {
+    const email = this.resetpwform.value.email;
+    const newpassword = this.resetpwform.value.newpassword;
+    const oldpassword = this.resetpwform.value.oldpassword;
+    const confirmpw = this.resetpwform.value.confirmpw;
+
+    const currentItems = this.dataService.getItem('items') || [];
+
+    const index = currentItems.findIndex((item: any) => item.email == email);
+    if((email == data.email)&& (data.password == oldpassword))
+    {
+        data.password=newpassword;
+        this.successalert = 'You have successfully reset password! '+data.username;
+    }
+    else if((index !== -1)&& (currentItems[index].password=oldpassword))
+    { 
+      currentItems[index].password=newpassword;
+      console.log(currentItems[index].password)
+      localStorage.setItem('items', JSON.stringify(currentItems));
+      this.successalert = 'You have successfully reset password! '+ currentItems[index].username;
+    }
+    else{
+          this.failalert = 'Invalid User!';
+          this.resetpwform.reset();
+    }
+  }
+  
+  get loginForm(){
     return this.loginform.controls;
   }
+
+  get forgetForm(){
+    return this.forgetpwform.controls;
+  }
+
+  get resetForm(){
+    return this.resetpwform.controls;
+  }
+  
 
 }
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -44,7 +44,8 @@ export class RegisterComponent implements OnInit {
     )
   }
 
-  alert:string='';
+  successalert:string='';
+  failurealert:string='';
 
   onSubmit(){
     console.log(this.registerform.value);
@@ -64,13 +65,23 @@ export class RegisterComponent implements OnInit {
       lastname:lastname, address:address, dob:dob, contact:contact, email:email, confirmpassword:confirmpassword};
 
       const currentItems = this.datastore.getItem('items') || []; 
-      currentItems.push(details);
-      this.datastore.setItem('items', currentItems);
-     
+      const emailExists = currentItems.some((item: { email: any; }) => item.email === email);
+      
+      
     if(this.registerform.valid)
     {
-      this.alert = 'Successfully registered !'+firstname;
-      this.registerform.reset();
+      if (!emailExists) {
+        currentItems.push(details);
+        this.datastore.setItem('items', currentItems);  
+        this.successalert = 'Successfully registered !'+firstname;
+        this.registerform.reset();
+      }
+      else{
+        this.failurealert = 'Invalid register, Email already exists !';
+        this.registerform.reset();
+      }
+      
+      
     }
     
       
@@ -85,3 +96,104 @@ export class RegisterComponent implements OnInit {
  
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import { Component, OnInit } from '@angular/core';
+// import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
+
+// import { CustomvalidationService } from '../customvalidation.service';
+// import { HandledataService } from '../handledata.service';
+
+
+// import { HttpClient } from '@angular/common/http';
+
+// @Component({
+//   selector: 'app-register',
+//   templateUrl: './register.component.html',
+//   styleUrls: ['./register.component.css']
+// })
+// export class RegisterComponent implements OnInit {
+  
+//   constructor(public fb:FormBuilder, private customvalidator:CustomvalidationService,
+//     private datastore:HandledataService)
+//   {
+    
+//   }
+
+//   registerform : FormGroup = new FormGroup ({});
+
+//   ngOnInit(): void {
+
+//     this.datastore.getDataFromJsonFile().subscribe(data => {
+//       localStorage.setItem('myData', JSON.stringify(data));
+//     });
+//     this.registerform = this.fb.group(
+//       {
+//         username : new FormControl('', Validators.required),
+//         password : new FormControl('', Validators.compose([Validators.required, this.customvalidator.patternValidator()])),
+//         firstname : new FormControl('', Validators.required),
+//         lastname : new FormControl('', Validators.required),
+//         address : new FormControl('', Validators.required),
+//         dob : new FormControl('', Validators.required),
+//         contact : new FormControl('', [Validators.required, Validators.pattern(/^\d{10}$/)]),
+//         email : new FormControl('', [Validators.required, Validators.email]),
+//         confirmpassword : new FormControl('', Validators.required),
+        
+//       },
+//       {validator: this.customvalidator.MatchPassword('password', 'confirmpassword')}
+//     )
+//   }
+
+//   alert:string='';
+
+//   onSubmit(){
+//     console.log(this.registerform.value);
+//     console.log(this.registerform.valid);
+
+//     const username = this.registerform.value.username;
+//     const password = this.registerform.value.password;
+//     const firstname = this.registerform.value.firstname;
+//     const lastname = this.registerform.value.lastname; 
+//     const address = this.registerform.value.address;
+//     const dob = this.registerform.value.dob;
+//     const contact = this.registerform.value.contact;
+//     const email = this.registerform.value.email;
+//     const confirmpassword = this.registerform.value.confirmpassword;
+
+//     const details = { username: username, password: password, firstname:firstname,
+//       lastname:lastname, address:address, dob:dob, contact:contact, email:email, confirmpassword:confirmpassword};
+
+//       const currentItems = this.datastore.getItem('items') || []; 
+//       currentItems.push(details);
+//       this.datastore.setItem('items', currentItems);
+     
+//     if(this.registerform.valid)
+//     {
+//       this.alert = 'Successfully registered !'+firstname;
+//       this.registerform.reset();
+//     }
+    
+      
+    
+//   }
+
+//   get registerForm()
+//   {
+//       return this.registerform.controls;
+//   }
+
+ 
+
+// }
